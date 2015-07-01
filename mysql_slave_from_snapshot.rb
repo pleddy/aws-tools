e8f45d08fd5d9c6101ee12e42712282208795395
pleddy <pleddy@users.noreply.github.com>
2015-07-01 16:02:56 -0700

    Update mysql_slave_from_snapshot.rb

--- mysql_slave_from_snapshot.rb
+++ mysql_slave_from_snapshot.rb
@@ -4,77 +4,97 @@ require 'pp'
 require 'aws-sdk'
 require 'json'
 
-
 # get cli arguments
-
 if ( ! (ARGV[0]) )
   puts "Usage: snapshot required"
   exit 
 end
 my_snapshot = ARGV[0]
 
-
 # initialize
-creds = JSON.load(File.read('secrets.json'))
-Aws.config[:credentials] = Aws::Credentials.new(creds['aws']['creds']['aws_access_key_id'], creds['aws']['creds']['aws_secret_access_key'])
-repl_username = creds['mysql']['replication_user']
-repl_password = creds['mysql']['replication_password']
-rds_instance = creds['mysql']['rds_instance']
-region_name = 'us-east-1'
-
+my_creds = JSON.load(File.read('secrets.json'))
+Aws.config[:credentials] = Aws::Credentials.new(my_creds['aws']['creds']['aws_access_key_id'], my_creds['aws']['creds']['aws_secret_access_key'])
+my_mysql_replication_user = my_creds['mysql']['mysql_replication_user']
+my_mysql_replication_password = my_creds['mysql']['mysql_replication_password']
+my_mysql_master_user = my_creds['mysql']['mysql_master_user']
+my_mysql_master_password = my_creds['mysql']['mysql_master_password']
+my_region_name = 'us-east-1'
 
 # get the aws accout id for arn building
+print 'getting aws acct number: '
 begin
-  iam_resource = Aws::IAM::Resource.new( client: Aws::IAM::Client.new(region: region_name) )
-  aws_acct_id = iam_resource.current_user.arn.match(/\d+/).to_s
+  my_iam_resource = Aws::IAM::Resource.new( client: Aws::IAM::Client.new(region: my_region_name) )
+  my_aws_acct_id = my_iam_resource.current_user.arn.match(/\d+/).to_s
+  puts my_aws_acct_id
 rescue
   p 'gathering account information failed'
   exit
 end
 
-
 # get the name of the db instance that parented the snapshot, and the snapshot tags that define master replication
+print 'getting parent db instance name and master db info via snapshot tags: '
 begin
-  rds_client = Aws::RDS::Client.new(region: region_name)
-  resp = rds_client.list_tags_for_resource(options = {resource_name: "arn:aws:rds:us-east-1:#{aws_acct_id}:snapshot:#{my_snapshot}"})
-  original_name = rds_client.describe_db_snapshots({db_snapshot_identifier: my_snapshot})['db_snapshots'][0].db_instance_identifier
+  my_rds_client = Aws::RDS::Client.new(region: my_region_name)
+  my_resp = my_rds_client.list_tags_for_resource(options = {resource_name: "arn:aws:rds:us-east-1:#{my_aws_acct_id}:snapshot:#{my_snapshot}"})
+  my_original_name = my_rds_client.describe_db_snapshots({db_snapshot_identifier: my_snapshot})['db_snapshots'][0].db_instance_identifier
+  puts my_original_name
 rescue
   p 'gathering snapshot info failed'
   exit
 end
 my_tags = {}
-resp[0].each do |my_tag| my_tags[my_tag.key] = my_tag.value end
-#pp my_tags
-
+my_resp[0].each do |my_tag| my_tags[my_tag.key] = my_tag.value end
+p my_tags
+my_master_host = my_tags['master_host']
+my_master_log_file = my_tags['master_log_file']
+my_master_log_pos = my_tags['master_log_pos']
+my_db_subnet_group_name = my_tags['db_subnet_group_name']
 
 # create a new db instance from the snapshot
-ymdhm = Time.new.strftime('%Y%m%d%H%M')
-my_name = original_name + '-' + ymdhm
+print 'restore snapshot: '
+my_ymdhm = Time.new.strftime('%Y%m%d%H%M')
+my_name = my_original_name + '-' + my_ymdhm
+puts my_name
 begin
-  resp = rds_client.restore_db_instance_from_db_snapshot({
+  resp = my_rds_client.restore_db_instance_from_db_snapshot({
     db_instance_identifier: my_name, 
     db_snapshot_identifier: my_snapshot, 
     db_instance_class: 'db.m1.small', 
+    db_subnet_group_name: my_db_subnet_group_name,
     #tags: [{key: "a", value: "001"}]
   })
 rescue Aws::RDS::Errors::ServiceError
   p 'snapshot restore failed'
   exit
 end
-#pp resp
 
-# get an resource object for the new db instance, use it to monitor if the db instance is up yet
-my_resource = Aws::RDS::Resource.new(client: rds_client)
-my_db_instance = my_resource.db_instance(my_name)
-
-my_db_instance.wait_until(max_attempts:30, delay:60) do |instance| 
-  instance.db_instance_status == 'available'
-  print '.'
+# monitor if the db instance is up yet
+my_status = nil
+puts 'waiting for db instance to boot, 30 second updates follow'
+while ( my_status != 'available' ) do
+  my_resp = my_rds_client.describe_db_instances({db_instance_identifier: my_name})
+  my_status = my_resp.db_instances[0].db_instance_status
+  print my_status
+  puts '...'
+  sleep 30
 end
 
+# fetch endpoint for RDS instance using name of rds instance's name
+print 'getting endpoint of new db instance: '
+my_rds_resource = Aws::RDS::Resource.new(client: my_rds_client)
+my_db_instance = my_rds_resource.db_instance(my_name)
+my_rds_endpoint = my_db_instance.endpoint.address
+p my_rds_endpoint
 
+# set master
+p 'set master for replication'
+my_result = %x[mysql -e "CALL mysql.rds_set_external_master ('#{my_master_host}', 3306, '#{my_mysql_replication_user}', '#{my_mysql_replication_password}', '#{my_master_log_file}', #{my_master_log_pos}, 0);" -h #{my_rds_endpoint} -u #{my_mysql_master_user} -p#{my_mysql_master_password} 2>/dev/null]
+p my_result
 
+# start slave replication
+p 'start replication'
+my_result = %x[mysql -e 'CALL mysql.rds_start_replication;' -h #{my_rds_endpoint} -u #{my_mysql_master_user} -p#{my_mysql_master_password} 2>/dev/null]
+p my_result
 
-puts 'END'
-
+p 'completed, new cloned db instance should be replicating master'
 
